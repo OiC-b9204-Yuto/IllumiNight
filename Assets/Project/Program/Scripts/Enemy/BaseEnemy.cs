e4e07ad81573a80fd177f031703c05ae6f736dd1
OiC-b9204-Yuto <71886577+OiC-b9204-Yuto@users.noreply.github.com>
2022-05-24 17:55:45 +0900

    敵の基底クラスを作成中

---
+++ Assets/Project/Program/Scripts/Enemy/BaseEnemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+// 敵のベースクラス（作成中：竹中）
+//  移動と攻撃を変更できるようにコードを書く
+//  Reactiveに頼りすぎない（コードが追いづらくなるのを防ぐ）
+
+public class BaseEnemy : MonoBehaviour
+{
+    [SerializeField] protected int _initLifePoint;
+
+    // 現在のライフポイント
+    [SerializeField] protected IntReactiveProperty _currentLifePoint;
+    public IReadOnlyReactiveProperty<int> CurrentLifePoint => _currentLifePoint;
+
+    // ダメージを受けるまでに必要な戦闘時間
+    [SerializeField] protected float _requiredBattleTime;
+    public float RequiredBattleTime => _requiredBattleTime;
+
+    // 死亡用フラグ
+    [SerializeField] protected BoolReactiveProperty _isDead;
+    public IReadOnlyReactiveProperty<bool> IsDead => _isDead;
+
+
+
+    protected void Initialize()
+    {
+        _currentLifePoint = new IntReactiveProperty(_initLifePoint);
+        _isDead = new BoolReactiveProperty(false);
+        CurrentLifePoint.Where(_ => _ <= 0).Subscribe(_ => _isDead.Value = true); 
+    }
+
+    void Start()
+    {
+        Initialize();
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
