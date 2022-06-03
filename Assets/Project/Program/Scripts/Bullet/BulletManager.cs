f13d6509a2579b69321e748c67c4c65f2c0fd36b
OiC-b9204-Yuto <71886577+OiC-b9204-Yuto@users.noreply.github.com>
2022-06-03 15:00:20 +0900

    弾のクラスファイル作成

---
+++ Assets/Project/Program/Scripts/Bullet/BulletManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class BulletManager : MonoBehaviour
+{
+    ObjectPool<Bullet> pool;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
