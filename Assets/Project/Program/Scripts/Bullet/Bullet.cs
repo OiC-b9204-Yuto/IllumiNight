9a0a748d2c2f38188aa8942f44ff6d3ede7dbba9
OiC-b9204-Yuto <71886577+OiC-b9204-Yuto@users.noreply.github.com>
2022-06-10 13:42:46 +0900

    弾の仮処理記述

--- Assets/Project/Program/Scripts/Bullet/Bullet.cs
+++ Assets/Project/Program/Scripts/Bullet/Bullet.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody),typeof(Collider))]
 public class Bullet : MonoBehaviour
 {
-    
-    void Start()
+    Rigidbody _rigidbody;
+    float _speed;
+    void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Setup(Vector3 pos, Vector3 dir, float speed)
     {
-        
+        transform.position = pos;
+        transform.rotation = Quaternion.LookRotation(dir);
+        _speed = speed;
+    }
+
+    void FixedUpdate()
+    {
+        _rigidbody.velocity = _speed * Time.fixedDeltaTime * transform.forward;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            // var player = GetComponent<Player>(); 
+            // player.TakeDamage();
+            gameObject.SetActive(false);
+        }
+        else if (other.tag == "Stage")
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
