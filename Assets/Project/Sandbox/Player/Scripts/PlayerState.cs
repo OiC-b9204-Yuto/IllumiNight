012a147961032b6ff3e611e80fb7616e38d47b94
OiC-b9336-Naoki <b9336@oic.jp>
2022-06-27 17:05:12 +0900

    BaseEnemyを模倣する形でPlayerに体力と死亡フラグを置

---
+++ Assets/Project/Sandbox/Player/Scripts/PlayerState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class PlayerState : MonoBehaviour
+{
+    [SerializeField] private int _initLifePoint;
+
+    [SerializeField] private IntReactiveProperty _currentLifePoint;
+    /// <summary>
+    /// 現在のライフポイント
+    /// </summary>
+    public IReadOnlyReactiveProperty<int> CurrentLifePoint => _currentLifePoint;
+
+    [SerializeField] protected BoolReactiveProperty _isDead;
+    /// <summary>
+    /// 死亡用フラグ
+    /// </summary>  
+    public IReadOnlyReactiveProperty<bool> IsDead => _isDead;
+    void Awake()
+    {
+        _currentLifePoint = new IntReactiveProperty(_initLifePoint);
+        _isDead = new BoolReactiveProperty(false);
+        CurrentLifePoint.Where(_ => _ <= 0).Subscribe(_ => _isDead.Value = true);
+    }
+    public void TakeDamage(int damage)
+    {
+        _currentLifePoint.Value -= damage;
+    }
+}
